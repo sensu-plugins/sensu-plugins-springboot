8f6a6dae27e14851cd47bbb845cca5ce3af5c8fb
Matt Jones <mattjones@yieldbot.com>
2015-05-20 17:55:28 -0400

    update repo

--- lib/sensu-plugins-springboot.rb
+++ lib/sensu-plugins-springboot.rb
@@ -1,7 +1,15 @@
+
+require 'sensu-plugins-springboot/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsSpringboot
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
